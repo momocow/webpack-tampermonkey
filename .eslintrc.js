d826c1b22f412f51095090b7a1ad2bfde61afb15
MomoCow <momocow.me@gmail.com>
2021-07-08 14:05:21 +0800

    :wrench: Add docs pipeline.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,14 @@ const config = require('eslint-config-standard-typescript-prettier');
 
 module.exports = {
   ...config,
+  plugins: [...config.plugins, "eslint-plugin-tsdoc"],
   parserOptions: { project: './tsconfig.json' },
   rules: {
     ...config.rules,
     '@typescript-eslint/no-explicit-any': 'error',
     'max-len': ['error', { code: 80 }],
     "no-useless-constructor": "off",
-    "@typescript-eslint/no-useless-constructor": ["error"]
+    "@typescript-eslint/no-useless-constructor": ["error"],
+    "tsdoc/syntax": "warn"
   }
 };
